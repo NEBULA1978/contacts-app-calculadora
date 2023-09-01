86fdd8156a3530027679f0f84a3c31d61396437f
NEBULA1978 <rpascualey@gmail.com>
2023-09-02 00:22:04 +0200

    La foto de inicio esta pequeña pero va bien

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 
 <div class="welcome d-flex align-items-center justify-content-center">
   <div class="text-center">
+    <!-- Agregamos el botón para volver al index.php anterior -->
+    <div class="text-center">
+      <a class="btn btn-lg btn-primary" href="../index.php">Volver al Inicio</a>
+    </div>
     <h1>Store Your Contacts Now</h1>
     <a class="btn btn-lg btn-dark" href="register.php">Get Started</a>
   </div>
 </div>
-<a class="btn btn-lg btn-dark" href="imc/index.php">Ir a IMC</a>
 
 <?php require "partials/footer.php" ?>
+<a class="btn btn-lg btn-dark" href="imc/index.php">Ir a IMC</a>
--- welcome.js
+++ welcome.js
@@ -3,12 +3,10 @@ const welcome = document.querySelector(".welcome");
 const navbarToggle = document.querySelector("#navbarNav");
 
 const resizeBakgroundImg = () => {
-  const height = window.innerHeight - navbar.clientHeight;
+  const height = (window.innerHeight - navbar.clientHeight) * 2; // Ajusta el valor multiplicador
   welcome.style.height = `${height}px`;
 };
 
-
-navbarToggle.ontransitionend = resizeBakgroundImg;
-navbarToggle.ontransitionstart = resizeBakgroundImg;
-window.onresize = resizeBakgroundImg;
-window.onload = resizeBakgroundImg;
+// Llama a la función para ajustar la imagen de fondo cuando la página se carga y cuando cambia el tamaño de la ventana.
+window.addEventListener("load", resizeBakgroundImg);
+window.addEventListener("resize", resizeBakgroundImg);
